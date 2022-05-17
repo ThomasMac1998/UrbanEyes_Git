ada517663e6ab95ac66d9ef082efa04507577c40
Thomas MacGillavry <ThomasMac@eduroam-078-104-023-139.wlan.univie.ac.at>
2022-05-17 12:41:45 +0200

    Some changes I made to Andrew's original script. Added the additional model diagonistics with performance package.

--- eye size models.R
+++ eye size models.R
@@ -1,23 +1,45 @@
+#Load required packages. 
 library(ggplot2)
 library(dplyr)
 library(lme4)
 
+#Inspect dataframe. 
+Final_Dataset
+str(Final_Dataset)
 
+#A few variables are loaded as characters. 
+#Set characters to factors. 
+Final_Dataset$site <- as.factor(Final_Dataset$site)
+Final_Dataset$habitat <- as.factor(Final_Dataset$habitat)
+Final_Dataset$picture_nr <- as.factor(Final_Dataset$picture_nr)
+Final_Dataset$species <- as.factor(Final_Dataset$species)
+Final_Dataset$bird_ring_number <- as.factor(Final_Dataset$bird_ring_number)
+Final_Dataset$Observer <- as.factor(Final_Dataset$Observer)
 
+#Check if all characters are set to factors. 
+str(Final_Dataset)
+
+install.packages("performance")
+
+#Model Selection using 'performance'. 
+#Load the performance package. 
+library(performance)
+
+#Load the first model. 
 model1 <- lmer(mean_diameter ~ habitat + species + scaled_mass_index + 
                  habitat*species + scaled_mass_index*species + scaled_mass_index*habitat + 
                  (1|Batch) + (1|site), 
-               data = final_clean, REML = F) 
+               data = Final_Dataset, REML = F) 
 
 drop1(model1, test = "Chisq")
-
 summary(model1)
 
 ## interaction of habitat and SMI non-significant so removed from final model
 
+#Load second model with dropped interaction habitat*SMI. 
 model2 <- lmer(mean_diameter ~ habitat + species + scaled_mass_index + 
                  habitat*species + scaled_mass_index*species + (1|Batch) + (1|site), 
-               data = final_clean, REML = F) 
+               data = Final_Dataset, REML = F) 
 drop1(model2, test = "Chisq")
 summary(model2)
 
@@ -33,13 +55,6 @@ qqline(residuals(model2))
 
 shapiro.test(residuals(model2))
 
-ggsave(path = "path",filename = "Histogram_residuals.jpeg", device = "jpeg", dpi=700)
-
-
-
-
-
-
 # use this anova function to add explanatory variables to the final model (model1)
 # and can see their statistical significance
 
@@ -52,6 +67,32 @@ ggsave(path = "path",filename = "Histogram_residuals.jpeg", device = "jpeg", dpi
 # summary function to see direction in response (i.e. corrections from baseline coefficient)
 # I need the p-value of the terms included in the final model (model2) from drop 1 command 
 # statistical significance of the p-values not in the final models (from model1 - i.e. SMI p-value = 0.2642).
+
+###
+#Conduct additional model checks with check_model() and model_performance(). 
+###
+
+#Load first model. 
+model1 <- lmer(mean_diameter ~ habitat + species + scaled_mass_index + 
+                 habitat*species + scaled_mass_index*species + scaled_mass_index*habitat + 
+                 (1|Batch) + (1|site), 
+               data = Final_Dataset, REML = F) 
+
+#Check model and calculate AIC, AICc, BIC, etc. 
+check_model(model1)
+model_performance(model1)
+
+#Load second model. 
+model2 <- lmer(mean_diameter ~ habitat + species + scaled_mass_index + 
+                 habitat*species + scaled_mass_index*species + (1|Batch) + (1|site), 
+               data = Final_Dataset, REML = F) 
+check_model(model2)
+model_performance(model2)
+
+#Comparing model1 and model2. 
+test_performance(model1, model2)
+
+
 
 
 
